04c2415033b27bc2efa7a283b41d15d294e51717
Ben Winter <ben.g.winter@gmail.com>
2013-12-03 21:26:00 -0500

    fixed tweet_streaming method which had issue with geo and place coordinates

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -4,10 +4,3 @@ $twitter_client = Twitter::Streaming::Client.new do |config|
   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
   config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
 end
-
-$twitter_client_2 = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-end
